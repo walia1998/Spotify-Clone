efc02c0cac01b9ac31adda301c5057c96a8b373a
Ashish Walia <waliaashu1998@gmail.com>
2024-08-21 13:36:14 +0530

    Add Song

--- Spotify-admin/src/main.jsx
+++ Spotify-admin/src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from "react";
+import ReactDom from 'react-dom/client'
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>
 );
